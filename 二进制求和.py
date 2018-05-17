dc0f68a978f940bd0b48c9c3b39a324ce5647a55
LXCuup <171059985@qq.com>
2018-05-17 17:25:45 +0800

    二进制求和其他方法

--- 二进制求和.py
+++ 二进制求和.py
@@ -29,7 +29,39 @@ class Solution:
         	answer += ans[i]*(10**(i))
         return str(answer)
 
-        
+class Solution:
+    def addBinary(self, a,b):
+        # 执行用时68ms
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        def dictobin(x):
+            li = x[::-1]
+            dic_num = 0
+            for i in range(len(li)):
+                dic_num += int(li[i]) * 2**i
+            return dic_num
+
+        dic_a = dictobin(a)
+        dic_b = dictobin(b)
+        dic_c = dic_a + dic_b
+        bin_c = str(bin(dic_c))[2:]
+        return bin_c
+
+class Solution:
+    def addBinary(self, a,b):
+        # 执行用时56ms
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        dic_a = int(a,2)
+        dic_b = int(b,2)
+        bin_c = bin(dic_a + dic_b)
+        return bin_c[2:]       
 
 s = Solution()
 print (s.addBinary('11','1')) # 100
